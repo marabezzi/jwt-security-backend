037a45a58a0df2ff53ce58f0c1ac2afa7ad5b82b
Guilherme Clerice <61362302+marabezzi@users.noreply.github.com>
2023-01-07 22:35:03 -0300

    Inicial

---
+++ src/main/java/io/marabezzi/jwtsecuritycrud/JwtSecurityCrudApplication.java
@@ -0,0 +1,13 @@
+package io.marabezzi.jwtsecuritycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityCrudApplication.class, args);
+	}
+
+}
